bed592df154fbac7e831dd183691a2b406a8f294
samahthwib <samah.thweib@hotmail.com>
2020-02-20 15:20:07 +0200

    added Q3 solution

--- app.js
+++ app.js
@@ -51,11 +51,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    
+var A=sum(a,b)[0];
+var A1=sum(A,c)[0];
+var B=multiply(a,b)[0];
+var B1=multiply(B,c)[0];
+return [A1,B1,`${a} and ${b} and ${c} sum to ${A1}.`,`The product of ${a} and ${b} and ${c} is ${B1}.`]
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
