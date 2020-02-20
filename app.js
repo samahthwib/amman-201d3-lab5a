1a9fe2f41fdd14e379803d12abce21013a0a877e
samahthwib <samah.thweib@hotmail.com>
2020-02-20 14:32:37 +0200

    added Q1 solution

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    var sumNum =a+b;
+    return [sumNum,`The sum of ${a} and ${b} is ${sumNum}.`];
 }
 
-// Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+// Here is the test for
+ //sum(); //uncomment it to run it
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -31,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ //testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,7 +50,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
